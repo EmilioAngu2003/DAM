7dc29f7eafedac962a904f2e5ddcb2799ea074b6
Emilio <emilio.angu.2003@gmail.com>
2024-02-17 20:28:41 +0100

    Acceso a Datos - organizado

--- DAM_02/Acceso a Datos/segunda evaluacion/Ejercicio Examen/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module dam02.accesoadatos {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires java.sql;
-
-
-    opens dam02.accesoadatos to javafx.fxml;
-    exports dam02.accesoadatos;
-}
