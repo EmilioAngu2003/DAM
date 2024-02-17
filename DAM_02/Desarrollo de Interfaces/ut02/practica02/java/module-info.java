8eea6d1631725e13471d0b6cc0cf2b458d4147d4
Emilio <emilio.angu.2003@gmail.com>
2024-02-17 20:33:09 +0100

    Desarrollo de Interfaces - organizado

--- DAM_02/Desarrollo de Interfaces/ut02/practica02/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module ut02.practica02 {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires java.base;
-
-    opens ut02.practica02 to javafx.fxml;
-    exports ut02.practica02;
-}
--- DAM_02/Desarrollo de Interfaces/ut02/practica03/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module ut02.practica03 {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires java.base;
-
-    opens ut02.practica03 to javafx.fxml;
-    exports ut02.practica03;
-}
